3f132b2d4ec5fede6760580c496b386d43a42926
dDaijin <30october03@gmail.com>
2024-12-16 11:20:44 +0200

    homework2

---
+++ lesson1/homework2.py
@@ -0,0 +1,55 @@
+#
+# string = 'get'
+# integer = 2
+# bool = True
+# list = ['pol', 'sol', 'dol']
+# dict = {'sam': 6, 'den': 7}
+# tuple = ['ssss', 'sed', 77]
+# x = None
+#
+# print(string == 'get')
+# print(integer >= 4)
+#
+#
+
+#Робота з рядками:
+
+# num_str = 125
+# num_str = str(num_str)
+# print(type(num_str))
+#
+# message ='Hi my name is Python'
+# message = message.replace('y', '0' )
+# message = message.replace('i', '1')
+# print(message)
+#
+#
+# split_test = 'This is a split Test'
+# split_test = split_test.split()
+# print(split_test)
+# string_join = " ".join(split_test)
+# print(string_join)
+# print(len(string_join))
+#
+
+
+# Робота зі списками:
+# list_append = [1,2,3]
+# list_append.append(4)
+# list_append.append(5)
+# print(list_append)
+#
+# list_extend = [4,5,6]
+# list_extend.extend([7,8,9])
+# list_extend = list_extend.index(6)
+# print(list_extend)
+# print(len(list_append))
+
+
+# Робота зі словниками
+# dict_test = {'car': 'Toyota', 'price': 4900, 'where': 'EU'}
+# print(dict_test['car'])
+# print(dict_test['where'])
+# print(dict_test.keys())
+# print(dict_test.values())
+# print(dict_test.items())
