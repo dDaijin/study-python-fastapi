d2e374926e121a6b502ec1aa8c18380028ebfeff
dDaijin <30october03@gmail.com>
2024-12-18 09:57:48 +0200

    hw4

---
+++ lesson1/homework4.py
@@ -0,0 +1,40 @@
+# Робота із списками
+# list = [10, 20]
+# list.remove(10)
+# print(list)
+
+# Знаходження суми
+# list = [11,2,6,3]
+# print(sum(list))
+
+# Подвійні значення
+# list = [11,2,6,3]
+# for i in list:
+#     i *= 2
+#     print(i)
+
+# Робота із кортежами
+# kortej = ("яблуко", "банан", "апельсин")
+# print(kortej[0])
+# print(kortej[1])
+# print(kortej[2])
+
+# Об'єднання кортежів
+# a = (1,2,3,4)
+# b = (5,6,7,8)
+# c = a+b
+# print(c)
+
+# Робота із словниками
+# f1 = {"name": "Max Verstapen", "age": 27, "sport": "F1", "Team": "RedBull" }
+# print(f1)
+
+# Оновлення словника
+# book = {"Book1": 2023, "Book2": 302}
+# upd = {"Book3": 23}
+# book.update(upd)
+# print(book)
+
+# # Пошук значення
+# country = {"Poland": "Warsawa","Ukraine": "Kiev", "Makedonia": "Skopie"}
+# print(country.get("Ukraine"))
